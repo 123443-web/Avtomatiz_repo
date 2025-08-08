b0c84608b90cbb35bd79136066453d689d9d3763
123443-веб <estarostinapl@mail.ru>
2025-08-08 15:15:34 +0300

    Сделала дом

---
+++ Scripts.py
@@ -0,0 +1,25 @@
+def print_python():
+    print("Учу Python!")
+my_heigh = 172
+print(my_heigh)
+
+my_name = "Катя"
+my_name = "Катя Севостьянова"
+print(my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print("Ваш любимчик - "+ pet_name)
+print_python()
+
+def print_letter(let):
+    print(let, end='')
+
+print_letter('С')
+print_letter('т')
+print_letter('у')
+print_letter('д')
+print_letter('е')
+print_letter('н')
+print_letter('т')
+
+
